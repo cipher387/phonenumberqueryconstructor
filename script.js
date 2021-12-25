32128dc81ca65af0d64bac8c1021296cd166dac8
Cyb_detective <66636466+cipher387@users.noreply.github.com>
2021-12-25 15:02:09 +0300

    Greece, France, Belgium

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function generateVariants()
 {
 
     country = document.getElementById("countries").value;
+    var phone = document.getElementById("phonenumber").value;
+    searchQuery = "\"+" + phone + "\" OR ";
 
     if (country === "Germany") {
 
-        var phone = document.getElementById("phonenumber").value;
-        searchQuery = "\"+" + phone + "\" OR ";
         searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 3) + " " + phone.substr(5, 3) + " " + phone.substr(8, 2) + " " + phone.substr(10, 2) + "\" OR ";
         searchQuery = searchQuery + "\"" + phone.substr(0, 5) + " " + phone.substr(5, 3) + " " + phone.substr(8, 2) + " " + phone.substr(10, 2) + "\" OR ";
         searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 3) + " " + phone.substr(5, 3) + "-" + phone.substr(8, 2) + "-" + phone.substr(10, 2) + "\" OR ";
@@ -18,8 +18,7 @@ function generateVariants()
         document.getElementById("result2").innerHTML = "";
     
     } else if (country === "Argentine") {
-        var phone = document.getElementById("phonenumber").value;
-        searchQuery = "\"+" + phone + "\" OR ";
+        
         searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 4) + " " + phone.substr(5, 6) + "\" OR ";
         searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 4) + " " + phone.substr(5, 3) + " " + phone.substr(8, 3) + "\" OR ";
         searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 4) + ") " + phone.substr(5, 3) + "-" + phone.substr(8, 3) + "\" OR ";
@@ -32,6 +31,36 @@ function generateVariants()
         searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 3) + ") " + phone.substr(4, 3) + "-" + phone.substr(7, 3) + "\" OR ";
         searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 3) + ") " + phone.substr(4, 2) + "-" + phone.substr(6, 2) + "-" + phone.substr(8, 2) + "\"";
         document.getElementById("result2").innerHTML = searchQuery;
+
+    } else if (country === "France") {
+       
+        searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 1) + ")" + phone.substr(3, 1) + " " + phone.substr(4, 2)+ " " + phone.substr(6, 2)+ " " + phone.substr(8, 2)+ " " + phone.substr(10, 2) + "\" OR ";
+        searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 1)  + phone.substr(3, 1) + " " + phone.substr(4, 2)+ " " + phone.substr(6, 2)+ " " + phone.substr(8, 2)+ " " + phone.substr(10, 2) + "\" OR ";
+        searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 1) +")"+ phone.substr(3, 1) + " " + phone.substr(4, 2)+ "-" + phone.substr(6, 2)+ "-" + phone.substr(8, 2)+ "-" + phone.substr(10, 2) + "\" OR ";
+        searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 1) + phone.substr(3, 1) + " " + phone.substr(4, 2)+ "-" + phone.substr(6, 2)+ "-" + phone.substr(8, 2)+ "-" + phone.substr(10, 2) + "\"";
+        document.getElementById("result").innerHTML = searchQuery;
+        document.getElementById("result2").innerHTML = "";
+
+    } else if (country === "Belgium") {
+        searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 1) + ") " + phone.substr(3, 3) + " " + phone.substr(6, 2)+ " " + phone.substr(8, 2) + "\" OR ";
+        searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 1) + " " + phone.substr(3, 3) + " " + phone.substr(6, 2)+ " " + phone.substr(8, 2) + "\" OR ";
+        searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 1) + ") " + phone.substr(3, 3) + "-" + phone.substr(6, 2)+ "-" + phone.substr(8, 2) + "\" OR ";
+        searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 1) + ") " + phone.substr(3, 4) + "-" + phone.substr(7, 3) + "\"";
+        document.getElementById("result").innerHTML = searchQuery;
+        document.getElementById("result2").innerHTML = "";
+    
+
+    } else if (country === "Greece") {
+     searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 2) + ") " + phone.substr(3, 4) + "-" + phone.substr(7, 3) + "\" OR ";
+     searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 2) + " " + phone.substr(3, 3) + " " + phone.substr(6, 4) + "\" OR ";
+     searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 2) + ") " + phone.substr(3, 4) + "-" + phone.substr(7, 3) + "\"";
+     document.getElementById("result").innerHTML = searchQuery;
+     searchQuery = "\"+" + phone + "\" OR ";
+     searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 3) + ") " + phone.substr(4, 4) + "-" + phone.substr(8, 3) + "\" OR ";
+     searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " " + phone.substr(2, 3) + " " + phone.substr(4, 3) + " " + phone.substr(7, 4) + "\" OR ";
+     searchQuery = searchQuery + "\"" + phone.substr(0, 2) + " (" + phone.substr(2, 3) + ") " + phone.substr(4, 4) + "-" + phone.substr(8, 3) + "\"";
+     document.getElementById("result2").innerHTML = searchQuery;   
+
 
     } else if (country === "Russia") {
 
